90f64d67173dee6fed00afdc471bdb0e7db0ae24
Koren Ben Ezra <109624775+Koren-Ben-Ezra@users.noreply.github.com>
2023-01-14 15:22:45 +0200

    ; )

---
+++ maman 13/_2023a/Q1.java
@@ -0,0 +1,32 @@
+/**
+ * Written by Koren Ben Ezra
+ * In 2023a
+ */
+public class Q1 {
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+
+    public static int alternating (String s){
+        // Checks every even index if it is equals to the first number,
+        // if not, then one change is needed for that number,
+        // the other number that we need to change from is in the odd index,
+        // so we can simply ignore it xD.
+
+        int firstNum = s.charAt(0);
+        int amountOfChanges = 0;
+
+        for (int i = 2; i < s.length(); i+=2) {
+            if(s.charAt(i) != firstNum)
+                amountOfChanges++;
+        }
+
+        return amountOfChanges;
+    }
+
+    public static void main(String[] args){
+        System.out.println("expected 2: " + alternating("00011011"));
+        System.out.println("expected 3: " + alternating("00101011"));
+
+    }
+}
