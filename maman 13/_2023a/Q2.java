822d1195a66ebb301762dbe144a3f3b009eced36
Koren Ben Ezra <109624775+Koren-Ben-Ezra@users.noreply.github.com>
2023-01-14 15:01:26 +0200

    Add files via upload

---
+++ maman 13/_2023a/Q2.java
@@ -0,0 +1,9 @@
+public class Q2 {
+    /**
+     *
+     *     Question 2:
+     *     https://github.com/Koren-Ben-Ezra/Exams/tree/openU-JAVA/_2018/_2018a85/src
+     *
+     *
+     */
+}
---
+++ maman 13/_2023a/Q4.java
@@ -0,0 +1,9 @@
+public class Q4 {
+    /**
+     *
+     *
+     *     https://github.com/Koren-Ben-Ezra/Exams/blob/openU-JAVA/_2018/_2018b83/src/Q1.java
+     *
+     *
+     */
+}
